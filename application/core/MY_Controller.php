d7b549a9ad74324d07977f71b5a7b62693120d17
rockyhandsome <rakesh.shriwas88@gmail.com>
2017-06-29 13:38:24 +0530

    Code Structure 2

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 require_once(APPPATH.'libraries/REST_Controller.php');
 
